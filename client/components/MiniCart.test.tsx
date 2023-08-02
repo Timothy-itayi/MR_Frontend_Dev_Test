942fbf22718df820385dec58dbe66a7c844c7a39
Timothy itayi <timmytam10@gmail.com>
2023-08-02 23:25:06 +1200

    write tests for product.tsx

--- client/components/MiniCart.test.tsx
+++ client/components/MiniCart.test.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { render } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
 import MiniCart from './MiniCart'
 
 describe('MiniCart', () => {
